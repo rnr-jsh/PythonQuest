6c2531b464df4f2ebcf974bcc730c53cef2ee11f
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-12 23:28:31 +0800

    department_caption.py with necessary codes

--- day-01/TASKS/department_caption.py
+++ day-01/TASKS/department_caption.py
@@ -3,19 +3,18 @@
 
 # Write the code ↓ to read user's input.
 
-
 name = input("Indicate your name: ")
 clubDept = input("Enter your AWS Cloud Club department: ")
 galaxy = input("Specify the galaxy to which you belong: ")
+
+
+# Write the code ↓ to display the department's caption.
+
 
 print("\n\nYour Deparment's DP Blast Caption: \n")
 print("🚀Deploying...I'm " + name + " , representing the " + clubDept + ". I'm thrilled to commence our expedition into the vast and uncharted realm of the " + galaxy + ". Together, we'll navigate this exciting frontier of technology! 🌌🌟💻")
 
 
-
-
-
-# Write the code ↓ to display the department's caption.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
