025b89ce29088fba4b9c3088d238b1158b30c448
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-13 22:18:39 +0800

    check_palindrome.py with necessary codes

--- day-05/TASKS/check_palindrome.py
+++ day-05/TASKS/check_palindrome.py
@@ -1,21 +1,23 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
 
+print("PALINDROME CHECKER FOR ALF\n")
 
-
-
-
+word = input("Please, enter a word/s to check: ")
 
 
 # Write the code ↓ to check if the entered word is a palindrome.
 # Utilize string functions to compare the original word with its reverse.
 
-
-
-
-
+reverse = word[::-1]
 
 # Write the code ↓ to display whether the entered word is a palindrome or not.
+
+if(word == reverse):
+    print("'%s' is a palindrome." % word)
+else:
+    print("'%s' is not a palindrome." % word)
+
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
