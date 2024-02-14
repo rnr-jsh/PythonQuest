cf78fa3abec166d699659c3317ab19ba9fc712e7
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-14 21:43:46 +0800

    main.py with necessary codes

--- day-10/Submission/main.py
+++ day-10/Submission/main.py
@@ -1,10 +1,3 @@
-'''
-Write a program that prompts the user to input their name, age, and favorite food. Take this input, format it into a dictionary, 
-and then encode and save it to a file named "output.json" using the json.dumps function.
-
-NOTE: only use built-in libraries of python (do not use pandas etc.)
-'''
-
 import json
 
 name = input("Please, enter your name: ")
