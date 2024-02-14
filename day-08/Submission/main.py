b2e0e6a77963b24ba431a5af446aa60e4819f040
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-14 21:17:18 +0800

    main.py with necessary codes

--- day-08/Submission/main.py
+++ day-08/Submission/main.py
@@ -1,11 +1,3 @@
-'''
-Create a Python script that does the following:
-
-Reads the "example1.csv" file and prints the values in the first column.
-Reads the "example2.json" file and prints its contents.
-NOTE: only use built-in libraries of python (do not use pandas etc.)
-'''
-
 import csv
 import json
 
