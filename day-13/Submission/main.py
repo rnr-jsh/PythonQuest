eb26cf403f7dfc94b5d0456309715b4a4a182868
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-16 17:19:45 +0800

    Add files via upload

--- day-13/Submission/main.py
+++ day-13/Submission/main.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify  
+import csv
 
 app = Flask(__name__)
 
 csv_file = 'programming_languages.csv'
 
-def read_csv():
-    pass
+def read_csv(filename):
+    content = {}
+    i = 0
+    with open('programming_languages.csv', 'r') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            content[i] = line
+            i += 1
+    return content
 
 @app.route('/', methods=['GET'])
 def get_langs():
-    pass
+    csv_file = read_csv('programming_languages.csv')
+    return jsonify(csv_file)
 
 if __name__ == '__main__':
     app.run(debug=True)
