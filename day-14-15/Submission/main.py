9befb5ee74ac42891f485d1a441534f9a26af1e8
Rainier Joshua Reyes <112159215+rnr-jsh@users.noreply.github.com>
2024-02-17 20:36:44 +0800

    Add files via upload

--- day-14-15/Submission/main.py
+++ day-14-15/Submission/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify  
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -14,39 +14,56 @@ aws_services = [
 ]
 
 # base url
-@app.route('/', methods=[''])
+@app.route('/', methods=['GET'])
 def hello():
-    pass
+    return "Welcome to the AWS Services Simple REST API"
 
 
 # /aws_services/get_all
-@app.route('/', methods=[''])
+@app.route('/aws_services/get_all', methods=['GET'])
 def get_all():
-    pass
+    return jsonify(aws_services)
 
 
 # /aws_services/<int:service_id>
-@app.route('/', methods=[''])
-def get_service():
-    pass
+@app.route('/aws_services/<int:service_id>', methods=['GET'])
+def get_service(service_id):
+    service = next((service for service in aws_services if service['id'] == service_id), None)
+    if service is None:
+        return jsonify({'message': 'Service not found'}), 404
+    return jsonify(service)
 
 
 # /aws_services/add_service
-@app.route('/', methods=[''])
+@app.route('/aws_services/add_service', methods=['POST'])
 def add_service():
-    pass
+    new_service = {
+        "id": aws_services[-1]['id'] + 1,
+        "service": request.json['service']
+    }
+    aws_services.append(new_service)
+    return jsonify(new_service), 201
 
 
 # /aws_services/delete_service/<int:service_id>
-@app.route('/', methods=[''])
-def delete_service():
-    pass
+@app.route('/aws_services/delete_service/<int:service_id>', methods=['DELETE'])
+def delete_service(service_id):
+    service = next((service for service in aws_services if service['id'] == service_id), None)
+    if service is None:
+        return jsonify({'message': 'Service not found'}), 404
+    aws_services.remove(service)
+    return jsonify({'message': 'Service deleted successfully'})
 
 
 # /aws_services/update_service/<int:service_id>
-@app.route('/', methods=[''])
-def update_service():
-    pass
+@app.route('/aws_services/update_service/<int:service_id>', methods=['PUT'])
+def update_service(service_id):
+    service = next((service for service in aws_services if service['id'] == service_id), None)    
+    if service is None:
+        return jsonify({'message': 'Service not found'}), 404
+    service['service'] = request.json['service']
+    return jsonify(service)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
